22be33d3f2ac5ecc9be531f454af27902d996bc1
Raquel Gomes <rgomes1115@gmail.com>
2025-05-20 20:13:26 -0300

    Dev

--- exemplo_git_eclipse/src/exemplo/ExemploGit.java
+++ exemplo_git_eclipse/src/exemplo/ExemploGit.java
@@ -3,6 +3,6 @@ package exemplo;
 public class ExemploGit {
 
 	public static void main(String[] args) {
-		System.out.println("Ol  vcvcvc á git! df dfd fdf dfdf");
+		System.out.println("Olá DEVELOPER");
 	}
 }
